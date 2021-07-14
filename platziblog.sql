fb2ef5eb99ae7f1107df1ed7b24d69c94df00b36
Pablo Diaz de Leon <pablorios1603@gmail.com>
2021-07-14 15:01:29 -0500

    Clase 20 -> Create view y DDL ALTER

--- platziblog.sql
+++ platziblog.sql
@@ -28,9 +28,23 @@ CREATE TABLE IF NOT EXISTS `platziblog`.`people` (
   `city` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`person_id`))
 ENGINE = InnoDB
+AUTO_INCREMENT = 4
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_spanish2_ci;
 
+USE `platziblog` ;
+
+-- -----------------------------------------------------
+-- Placeholder table for view `platziblog`.`platzi_people`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `platziblog`.`platzi_people` (`person_id` INT, `last_name` INT, `first_name` INT, `address` INT, `city` INT);
+
+-- -----------------------------------------------------
+-- View `platziblog`.`platzi_people`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `platziblog`.`platzi_people`;
+USE `platziblog`;
+CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `platziblog`.`platzi_people` AS select `platziblog`.`people`.`person_id` AS `person_id`,`platziblog`.`people`.`last_name` AS `last_name`,`platziblog`.`people`.`first_name` AS `first_name`,`platziblog`.`people`.`address` AS `address`,`platziblog`.`people`.`city` AS `city` from `platziblog`.`people`;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
