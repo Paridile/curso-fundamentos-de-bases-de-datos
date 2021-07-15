0d2d42b80dcc1d21f203fef932e16d1a839898c8
Pablo Diaz de Leon <pablorios1603@gmail.com>
2021-07-15 11:51:53 -0500

    Clase 32 -> WHERE

--- queries.sql
+++ queries.sql
@@ -42,3 +42,29 @@ SELECT *  FROM usuarios LEFT JOIN posts
 ON usuarios.id = posts.usuario_id WHERE posts.usuario_id IS NULL UNION  
 SELECT *  FROM usuarios RIGHT JOIN posts
 ON usuarios.id = posts.usuario_id WHERE posts.usuario_id IS NULL;
+
+
+-----------------------------------------------
+
+--WHERE
+
+SELECT * FROM posts where id <50;
+SELECT * FROM posts where id >=50;
+
+SELECT * FROM posts where estatus='activo';
+
+SELECT * FROM posts where estatus!='activo';
+
+SELECT * FROM posts where titulo LIKE '%escandalo%';
+SELECT * FROM posts where titulo LIKE '%roja';
+
+SELECT * FROM posts where fecha_publicacion > '2025-01-01';
+SELECT * FROM posts where fecha_publicacion <= '2025-01-01';
+
+SELECT * FROM posts where fecha_publicacion BETWEEN '2025-01-01' AND '2025-12-31';
+
+SELECT * FROM posts where id BETWEEN 50 AND 60;
+
+SELECT * FROM posts where YEAR(fecha_publicacion) BETWEEN '2023' AND '2024';
+SELECT * FROM posts where MONTH(fecha_publicacion) = '04';
+
