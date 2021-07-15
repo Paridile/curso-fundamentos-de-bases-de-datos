7f13ed376c515a106b0021d550af6640c8d93c0d
Pablo Diaz de Leon <pablorios1603@gmail.com>
2021-07-15 10:16:38 -0500

    Clase 29 -> SELECT

--- queries.sql
+++ queries.sql
@@ -8,3 +8,11 @@ FROM people WHERE active = true
 GROUP BY city
 ORDER BY total DESC
 HAVING total >= 2;
+
+
+
+SELECT titulo, fecha_publicacion, estatus FROM posts;
+
+SELECT titulo AS encabezado, fecha_publicacion AS publicacion, estatus AS estado FROM posts;
+
+SELECT count(*) AS 'total posts' FROM posts;
